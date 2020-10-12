5d2f394d4419b1be1ab4044c5e38fa5024fe0f0e
gubookone <gubookone@gmail.com>
2020-10-12 11:24:34 +0900

    test

---
+++ Pods/Target Support Files/Pods-bitrise-bitriseUITests/Pods-bitrise-bitriseUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bitrise_bitriseUITests : NSObject
+@end
+@implementation PodsDummy_Pods_bitrise_bitriseUITests
+@end
---
+++ Pods/Target Support Files/Pods-bitrise/Pods-bitrise-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bitrise : NSObject
+@end
+@implementation PodsDummy_Pods_bitrise
+@end
